4f82f1d95cddf3a954ce57db71a55c158db0bf1e
William Baker <bbrk24@gmail.com>
2024-03-14 10:00:35 -0400

    remove spaces from data.json

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ mkdir -p dist/src/ 2>/dev/null
 set -e
 shopt -s globstar
 civet --js -c src/**/*.civet -o dist/src/.js
-js-yaml data.yml >dist/data.json
+js-yaml data.yml | tr -d '\n ' >dist/data.json
 cp index.html index.css dist/
 cp -r img/ dist/
