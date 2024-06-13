08106a38b8e6cd08ef8bc4a59e89528ab372c2a4
William Baker <bbrk24@gmail.com>
2024-06-12 20:44:43 -0400

    Add Wii U version

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-mkdir -p dist/src/ 2>/dev/null
+mkdir -p dist/src/ dist/data/ 2>/dev/null
 set -e
 shopt -s globstar
 civet --js -c src/**/*.civet -o dist/src/.js
-js-yaml data.yml | tr -d '\n ' >dist/data.json
+find data -name '*.yml' | while IFS= read -r filename
+do
+    js-yaml "$filename" | tr -d '\n ' >"dist/${filename%.*}.json"
+done
 cp index.html index.css favicon.ico dist/
 cp -r img/ dist/
